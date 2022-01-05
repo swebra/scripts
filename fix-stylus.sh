51ff80fa3430b357422b881f168d1372ecf8aa14
swebra <eric@swebra.com>
2022-01-05 10:57:02 -0700

    Update display of fix-stylus

--- fix-stylus.sh
+++ fix-stylus.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
 xsetwacom set "Wacom HID 4875 Pen stylus" mode absolute
-xsetwacom set "Wacom HID 4875 Pen stylus" MapToOutput eDP1
+xsetwacom set "Wacom HID 4875 Pen stylus" MapToOutput eDP-1
 xsetwacom set "Wacom HID 4875 Pen eraser" mode absolute
-xsetwacom set "Wacom HID 4875 Pen eraser" MapToOutput eDP1
+xsetwacom set "Wacom HID 4875 Pen eraser" MapToOutput eDP-1
 xsetwacom set "Wacom HID 4875 Finger touch" mode absolute
-xsetwacom set "Wacom HID 4875 Finger touch" MapToOutput eDP1
+xsetwacom set "Wacom HID 4875 Finger touch" MapToOutput eDP-1
